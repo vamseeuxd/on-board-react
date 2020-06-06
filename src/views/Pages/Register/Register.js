4a42ac9e38e0340063671e295067f41401b33c1b
vamseeuxd <vamsee.uxd@gmail.com>
2020-06-06 14:56:59 +0530

    refactored registration Page

--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -3,7 +3,6 @@ import {
   Button,
   Card,
   CardBody,
-  CardFooter,
   Col,
   Container,
   Form,
@@ -11,7 +10,12 @@ import {
   InputGroup,
   InputGroupAddon,
   InputGroupText,
-  Row
+  Nav,
+  NavItem,
+  NavLink,
+  Row,
+  TabContent,
+  TabPane,
 } from 'reactstrap';
 import {Link} from "react-router-dom";
 import {connect} from 'react-redux';
@@ -25,6 +29,8 @@ class Register extends Component {
     this.state = {
       disableCreatAccount: true,
       disableGoogleRegistration: true,
+      currentTab: 1,
+      roleForGoogle: '',
       form: {
         username: '',
         role: '',
@@ -46,8 +52,11 @@ class Register extends Component {
     const form = this.state.form;
     form[controlName] = controlValue;
     const disableCreatAccount = Object.values(form).filter(value => value.trim().length == '').length > 0;
-    const disableGoogleRegistration = form.role.trim().length == 0;
-    this.setState({form, disableCreatAccount, disableGoogleRegistration});
+    this.setState({form, disableCreatAccount});
+  }
+
+  toggle() {
+
   }
 
   render() {
@@ -56,112 +65,176 @@ class Register extends Component {
       <div className="app flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
-            <Col md="9"
-                 lg="7"
-                 xl="6">
+            <Col md="9">
+
               <Card className="mx-4">
                 <CardBody className="p-4">
-                  <Form>
-                    <h1>
-                      Register
-                      <Link to="/login">
-                        <button className="btn btn-link float-right">Back To Login</button>
-                        <div className="clearfix"></div>
-                      </Link>
-                    </h1>
-                    <p className="text-muted">Create your account</p>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-user"></i> <span className="text-danger">*</span>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text"
-                             onChange={(event) => this.updateFormValue(event.target.id, event.target.value)}
-                             id="username"
-                             defaultValue={this.state.form.username}
-                             placeholder="Username"
-                             autoComplete="off"/>
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-shield"></i> <span className="text-danger">*</span>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="select"
-                             onChange={(event) => this.updateFormValue(event.target.id, event.target.value)}
-                             id="role"
-                             defaultValue={this.state.form.role}
-                             autoComplete="off"
-                             name="select">
-                        <option value="">Select Role</option>
-                        {this.getRolesOptions()}
-                      </Input>
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>@ <span className="text-danger">*</span></InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text"
-                             onChange={(event) => this.updateFormValue(event.target.id, event.target.value)}
-                             id="email"
-                             defaultValue={this.state.form.email}
-                             placeholder="Email"
-                             autoComplete="off"/>
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i> <span className="text-danger">*</span>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="password"
-                             onChange={(event) => this.updateFormValue(event.target.id, event.target.value)}
-                             id="password"
-                             defaultValue={this.state.form.password}
-                             placeholder="Password"
-                             autoComplete="off"/>
-                    </InputGroup>
-                    <InputGroup className="mb-4">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i> <span className="text-danger">*</span>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="password"
-                             onChange={(event) => this.updateFormValue(event.target.id, event.target.value)}
-                             id="repeatPassword"
-                             defaultValue={this.state.form.repeatPassword}
-                             placeholder="Repeat password"
-                             autoComplete="off"/>
-                    </InputGroup>
-                    <p className="text-muted">
-                      Please provide required <span className="text-danger">*</span> information to create an Account
-                    </p>
-                    <Button color="success"
-                            disabled={this.state.disableCreatAccount}
-                            block>Create Account</Button>
-                  </Form>
+
+                  <h4>
+                    Register
+                    <Link to="/login">
+                      <button className="btn btn-link float-right">Back To Login</button>
+                      <div className="clearfix"></div>
+                    </Link>
+                  </h4>
+
+                  <Nav tabs
+                       className="flex-nowrap border-bottom-0"
+                       style={{overflowX: 'auto', overflowY: 'hidden'}}>
+                    <NavItem>
+                      <NavLink className={this.state.currentTab == 0 ? 'active text-center' : 'text-center'}
+                               onClick={() => this.setState({currentTab: 0})}>
+                        With Email
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink className={this.state.currentTab == 1 ? 'active text-center' : 'text-center'}
+                               onClick={() => this.setState({currentTab: 1})}>
+                        With Google
+                      </NavLink>
+                    </NavItem>
+
+                    <NavItem>
+                      <NavLink className={this.state.currentTab == 2 ? 'active text-center' : 'text-center'}
+                               onClick={() => this.setState({currentTab: 2})}>
+                        With Facebook
+                      </NavLink>
+                    </NavItem>
+
+                  </Nav>
+
+                  <TabContent activeTab={this.state.currentTab}>
+                    <TabPane tabId={0}>
+
+                      <Form>
+
+                        <p className="text-muted">Create your account with Email</p>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-user"></i> <span className="text-danger">*</span>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input type="text"
+                                 onChange={(event) => this.updateFormValue(event.target.id, event.target.value)}
+                                 id="username"
+                                 defaultValue={this.state.form.username}
+                                 placeholder="Username"
+                                 autoComplete="off"/>
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-shield"></i> <span className="text-danger">*</span>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input type="select"
+                                 onChange={(event) => this.updateFormValue(event.target.id, event.target.value)}
+                                 id="role"
+                                 defaultValue={this.state.form.role}
+                                 autoComplete="off"
+                                 name="select">
+                            <option value="">Select Role</option>
+                            {this.getRolesOptions()}
+                          </Input>
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>@ <span className="text-danger">*</span></InputGroupText>
+                          </InputGroupAddon>
+                          <Input type="text"
+                                 onChange={(event) => this.updateFormValue(event.target.id, event.target.value)}
+                                 id="email"
+                                 defaultValue={this.state.form.email}
+                                 placeholder="Email"
+                                 autoComplete="off"/>
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-lock"></i> <span className="text-danger">*</span>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input type="password"
+                                 onChange={(event) => this.updateFormValue(event.target.id, event.target.value)}
+                                 id="password"
+                                 defaultValue={this.state.form.password}
+                                 placeholder="Password"
+                                 autoComplete="off"/>
+                        </InputGroup>
+                        <InputGroup className="mb-4">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-lock"></i> <span className="text-danger">*</span>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input type="password"
+                                 onChange={(event) => this.updateFormValue(event.target.id, event.target.value)}
+                                 id="repeatPassword"
+                                 defaultValue={this.state.form.repeatPassword}
+                                 placeholder="Repeat password"
+                                 autoComplete="off"/>
+                        </InputGroup>
+                        <p className="text-muted">
+                          Please provide required <span className="text-danger">*</span> information to create an
+                          Account
+                        </p>
+                        <Button color="success"
+                                disabled={this.state.disableCreatAccount}
+                                block>Create Account</Button>
+                      </Form>
+
+                    </TabPane>
+                    <TabPane tabId={1}>
+                      <Form style={{minHeight: '372px'}}>
+                        <p className="text-muted">Create your account with Google</p>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-shield"></i> <span className="text-danger">*</span>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input type="select"
+                                 onChange={
+                                   (event) => {
+                                     const disableGoogleRegistration = event.target.value.trim().length == 0;
+                                     this.setState({roleForGoogle: event.target.value, disableGoogleRegistration});
+                                   }
+                                 }
+                                 id="role"
+                                 defaultValue={this.state.roleForGoogle}
+                                 autoComplete="off"
+                                 name="select">
+                            <option value="">Select Role</option>
+                            {this.getRolesOptions()}
+                          </Input>
+                        </InputGroup>
+                        <p className="text-muted">
+                          Please Select a role to Register with Google
+                        </p>
+
+                        <GoogleLogin
+                          className="float-right"
+                          clientId="1020592783279-dib7nfhpbecp4gluf277pkj072shfqaj.apps.googleusercontent.com"
+                          buttonText="Register with Google"
+                          theme="dark"
+                          disabled={this.state.disableGoogleRegistration}
+                          onSuccess={googleLoginResponse => props.verifyGoogleLogin(googleLoginResponse, props.history)}
+                          cookiePolicy={'single_host_origin'}
+                        />
+
+                      </Form>
+                    </TabPane>
+
+                    <TabPane tabId={2}>
+                      <Form style={{minHeight: '372px'}}>
+                        <p className="text-muted">Create your account with Facebook</p>
+                        <h4 className="text-warning">Under Development</h4>
+                      </Form>
+                    </TabPane>
+                  </TabContent>
+
                 </CardBody>
-                <CardFooter className="p-4">
-                  <Row>
-                    <Col xs="12"
-                         sm="12">
-                      <p className="text-muted">
-                        Please Select a role to Register with Google
-                      </p>
-                      <GoogleLogin
-                        clientId="1020592783279-dib7nfhpbecp4gluf277pkj072shfqaj.apps.googleusercontent.com"
-                        buttonText="Register with Google"
-                        theme="dark"
-                        disabled={this.state.disableGoogleRegistration}
-                        onSuccess={googleLoginResponse => props.verifyGoogleLogin(googleLoginResponse, props.history)}
-                        cookiePolicy={'single_host_origin'}
-                      />
-                    </Col>
-                  </Row>
-                </CardFooter>
               </Card>
             </Col>
           </Row>
